4fd1c3aa6602d3a7ac4706821741cb1f110da510
Stephane Gillon <stephanegillon@hotmail.con>
2022-10-11 14:44:58 +0200

    modify to use departures

--- app/routes/journeys/$stopIds.tsx
+++ app/routes/journeys/$stopIds.tsx
@@ -1,7 +1,7 @@
 import gtfsDepartures from "../../loaders/gtfsDepartures"
-import gtfsStops from "~/loaders/gtfsStops"
+// import gtfsStops from "~/loaders/gtfsStops"
 import SmartDisplay from "../../pages/SmartDisplay"
 
-export const loader = gtfsStops
+export const loader = gtfsDepartures
 
 export default SmartDisplay
