8a392858eed55a2f38b0cd6afe59a781905ed8bd
Stephane Gillon <stephanegillon@hotmail.con>
2022-10-11 14:45:41 +0200

    pause display data

--- app/pages/SmartDisplay.tsx
+++ app/pages/SmartDisplay.tsx
@@ -3,23 +3,23 @@ import { useLoaderData } from "@remix-run/react";
 function SmartDisplay() {
 
     const data = useLoaderData()
-
+    
     /* option possible */
     // if (!stopIds) {
     //     return (<div>erreur</div>)
     // }
 
-    const { name } = data ? data : {} // sort version : const { name } = data || {}
+    //const { name } = data ? data : {} // sort version : const { name } = data || {}
 
     return (
         <div>
-            {data ? 
+            {/* {data ? 
                 (<div>
                     <h1>{`Le stop name est : ${name}`}</h1>
                 </div>
                 ) : (
                 <h1>Error not found</h1>
-                )}
+                )} */}
         </div>
     )
   }
