1f496a3c1207a49a6ad9dedbd3fac4899ee5524e
Stephane Gillon <stephanegillon@hotmail.con>
2022-10-11 17:35:02 +0200

    modify to use data from the new loader departureForStopId
    Date format must be changed to display hours and minutes

--- app/pages/SmartDisplay.tsx
+++ app/pages/SmartDisplay.tsx
@@ -3,23 +3,46 @@ import { useLoaderData } from "@remix-run/react";
 function SmartDisplay() {
 
     const data = useLoaderData()
+    console.log("data : ", data);
+
+    const {stopDetails, departures} = data
+    const {name} = stopDetails || {}
+    
     
     /* option possible */
     // if (!stopIds) {
     //     return (<div>erreur</div>)
     // }
 
-    //const { name } = data ? data : {} // sort version : const { name } = data || {}
+    //const { name } = data ? data : {} // short version : const { name } = data || {}
 
     return (
         <div>
-            {/* {data ? 
-                (<div>
-                    <h1>{`Le stop name est : ${name}`}</h1>
-                </div>
-                ) : (
-                <h1>Error not found</h1>
-                )} */}
+            {
+                stopDetails && stopDetails.name
+                ? (<h1> {name} </h1>)
+                : (<p>error no name</p>)
+            }
+            {
+                departures && departures.length
+                ? (<div>
+                    <h2>prochains départs : </h2>
+                    <ul>
+                      {departures.map((item: any, index: number) => {
+                        // console.log(typeof item.departureDateTime);
+                        const departureDateTimeLocaleString = new Date(item.departureDateTime).toLocaleDateString()
+                      return (<li key={index}>{`${departureDateTimeLocaleString} : ${item.headsign}`}</li>)
+                      }
+                      )
+                      }
+                    </ul>
+
+                    
+                  </div>
+
+                )
+                : (<p>error no departure</p>)
+            }
         </div>
     )
   }
