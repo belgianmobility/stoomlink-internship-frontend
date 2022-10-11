b600f6ba570b0be51210b07784261c2fb38640a4
Stephane Gillon <stephanegillon@hotmail.con>
2022-10-11 14:59:12 +0200

    Cleaning file
    remove queries not used
    remove console.log

--- app/loaders/gtfsDepartures.tsx
+++ app/loaders/gtfsDepartures.tsx
@@ -1,50 +1,9 @@
-export default async function gtfsDepartures({params}) {
-  const {stopIds} = params
-    /* query for info stop ID*/
-    // const query = `{
-    //     allGtfsStops(filter: {id: {in: ["${stopIds}"]}}) {
-    //       nodes {
-    //         name
-    //         id
-    //         gtfsConnectionsByStopId {
-    //           nodes {
-    //             gtfsRouteByRouteId {
-    //               color
-    //               longName
-    //               shortName
-    //               textColor
-    //             }
-    //           }
-    //         }
-    //       }
-    //     }
-    //   }
-    //   `;
+export default async function gtfsDepartures({ params }) {
+  const { stopIds } = params
 
+  const date = new Date()
 
-    const query = `{
-      allGtfsDepartures(first: 10, filter: {stopId: {in: ["${stopIds}"]}}) {
-        nodes {
-          departureDateTime
-          isFirstOfDay
-          isLastOfDay
-          realtimeDepartureDateTime
-          gtfsRouteByRouteId {
-            shortName
-            textColor
-            color
-            agencyId
-            longName
-          }
-          headsign
-        }
-      }
-    }
-    `;
-
-    const date = new Date()
-
-    const query2 = `{
+  const query = `{
         allGtfsDepartures(
           first: 10,
           orderBy: DEPARTURE_DATE_TIME_ASC,
@@ -71,21 +30,18 @@ export default async function gtfsDepartures({params}) {
       }
     }
     `;
-  
-    const response = await fetch('https://graphql.api.production.stoomlink.io/graphql', {
-      method: 'POST',
-      headers: { "Content-Type": 'application/json' },
-      body: JSON.stringify({
-        query: query2
-      }),
-    }).then(resp => resp.json())    
 
-    const data = response.data && response.data.allGtfsDepartures && response.data.allGtfsDepartures.nodes && response.data.allGtfsDepartures.nodes.length
+  const response = await fetch('https://graphql.api.production.stoomlink.io/graphql', {
+    method: 'POST',
+    headers: { "Content-Type": 'application/json' },
+    body: JSON.stringify({
+      query: query
+    }),
+  }).then(resp => resp.json())
+
+  const data = response.data && response.data.allGtfsDepartures && response.data.allGtfsDepartures.nodes && response.data.allGtfsDepartures.nodes.length
     ? response.data.allGtfsDepartures.nodes
     : null
-    console.log("response : ",response);
-    
-    console.log("data : ",data);
 
   return data
 }  
