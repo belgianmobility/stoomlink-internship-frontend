6e9bcf85c9a2ed19f6debbfc7ed625d016f6d671
Stephane Gillon <stephanegillon@hotmail.con>
2022-10-13 12:01:38 +0200

    remove comentaries + new indentation

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -1,24 +1,24 @@
 function Header(props: { stopDetails: any }) {
 
-    const {stopDetails} = props
+    const { stopDetails } = props
 
-    const {name, gtfsAgencyByAgencyId} = stopDetails || {} // --> header
-    const {name : agencyName} = gtfsAgencyByAgencyId || {} // --> header
+    const { name, gtfsAgencyByAgencyId } = stopDetails || {}
+    const { name: agencyName } = gtfsAgencyByAgencyId || {}
 
     return (
         <header>
             <div className="header-name">
                 {
                     name
-                    ? (<h1> {name} </h1>)
-                    : (<p>error no name</p>)
+                        ? (<h1> {name} </h1>)
+                        : (<p>error no name</p>)
                 }
             </div>
             <div className="header-agency">
                 {
                     agencyName
-                    ? (<div> {agencyName} </div>)
-                    : (<p>error no agency</p>)
+                        ? (<div> {agencyName} </div>)
+                        : (<p>error no agency</p>)
                 }
             </div>
         </header>
