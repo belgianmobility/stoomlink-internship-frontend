2601e8162ce9fafacb8ff197672107f7aec7af24
Stephane Gillon <stephanegillon@hotmail.con>
2022-10-18 11:36:59 +0200

    new loader stops for all directions

---
+++ app/loaders/gtfsStopsAllDirections.tsx
@@ -0,0 +1,46 @@
+export default async function gtfsStops({params}) {
+  const {stopIds} = params
+
+  const stopIdsArray = stopIds.split(',')
+  
+    const query = `{
+      allGtfsStops(filter: {id: {in: [${stopIdsArray.map((el) => (`"${el}"`)).join(',')}]}}) {
+        nodes {
+          name
+          id
+          gtfsAgencyByAgencyId {
+            name
+            id
+          }
+          gtfsConnectionsByStopId {
+            nodes {
+              gtfsRouteByRouteId {
+                color
+                longName
+                shortName
+                textColor
+              }
+            }
+          }
+        }
+      }
+    }
+      `;
+    
+    const response = await fetch('https://graphql.api.production.stoomlink.io/graphql', {
+      method: 'POST',
+      headers: { "Content-Type": 'application/json' },
+      body: JSON.stringify({
+        query: query
+      }),
+    }).then(resp => resp.json())
+
+    
+    
+
+    /* to read several stops */
+    const data = response.data && response.data.allGtfsStops && response.data.allGtfsStops.nodes && response.data.allGtfsStops.nodes.length
+    ? response.data.allGtfsStops.nodes
+    : null
+  return data
+}  
