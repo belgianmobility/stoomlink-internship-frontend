87fbf517bfcb541b483feb60eee9e1eb5e37fb4a
Stephane Gillon <stephanegillon@hotmail.con>
2022-10-20 10:39:25 +0200

    fix move ternary inside DIV

--- app/components/DepartureTimeHr.tsx
+++ app/components/DepartureTimeHr.tsx
@@ -6,11 +6,16 @@ function DepartureTimeHr(props: { departure: any }) {
     const departureTime = new Date(departureDateTime).toLocaleTimeString('fr-EU', { hour: '2-digit', minute: '2-digit' })
     
     return (
-        Date.parse(departureDateTime) //check if valide date format
-        ? (<div>
-            {departureTime}    
-        </div>)
-        : (<p>??:??</p>)
+        <div>
+            {
+                Date.parse(departureDateTime) //check if valide date format
+                ? (<div>
+                    {departureTime}    
+                </div>)
+                : (<p>??:??</p>) 
+            }
+        </div>
+        
     )
 }
 
