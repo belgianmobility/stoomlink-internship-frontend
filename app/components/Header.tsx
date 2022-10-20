0e79a54ebabd2cbbac42f179c3c861661db2abfc
Stephane Gillon <stephanegillon@hotmail.con>
2022-10-20 10:40:04 +0200

    Remove comments

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -2,9 +2,6 @@ function Header(props: { name: string }) {
 
     const { name } = props
 
-    // const { name, gtfsAgencyByAgencyId } = name || {}
-    // const { name: agencyName } = gtfsAgencyByAgencyId || {}
-
     return (
         <header>
             <div className="header-name">
@@ -14,13 +11,6 @@ function Header(props: { name: string }) {
                         : (<p>error no name</p>)
                 }
             </div>
-            {/* <div className="header-agency">
-                {
-                    agencyName
-                        ? (<div> {agencyName} </div>)
-                        : (<p>error no agency</p>)
-                }
-            </div> */}
         </header>
     )
 }
