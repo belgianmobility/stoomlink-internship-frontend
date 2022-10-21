03f6ecb158f56dc7955fe350077ddbb62adf59b3
Stephane Gillon <stephanegillon@hotmail.con>
2022-10-21 16:10:23 +0200

    remove unwanted data

--- app/loaders/gtfsStopsAllDirections.tsx
+++ app/loaders/gtfsStopsAllDirections.tsx
@@ -19,7 +19,6 @@ export default async function gtfsStops({params}) {
               gtfsRouteByRouteId {
                 id
                 color
-                longName
                 shortName
                 textColor
               }
