5e406e5dadd721179a3dfbb1b52c7b4508397ccb
Stephane Gillon <stephanegillon@hotmail.con>
2022-10-21 10:22:55 +0200

    Bug useState

--- app/components/Clock.tsx
+++ app/components/Clock.tsx
@@ -1,11 +1,45 @@
+import { useEffect, useState } from "react"
+
 function Clock() {
+    //const [today, setToday] = useState(new Date())
+    const [today, setToday] = useState(0)
+    console.log(today);
+    console.log("le type est" ,typeof today);
+    
+    
+    // useEffect(() => {
+    //     console.log("test into useEffect");
+        
+    //     const timer = setTimeout(() => {
+    //       console.log('This will run after 3 second!')
+    //     }, 3000);
+    //     return () => clearTimeout(timer);
+    //   }, []);
 
-    const today = new Date()
-    let hour = today.getHours()
-    let minute = today.getMinutes()
+    function refreshClock() {
+      setToday(new Date())
+    }
 
+    useEffect (() => {
+      console.log("test into useEffect");
+      // const timerId = setInterval(refreshClock, 1000)
+      // return function cleanup() {
+      //   clearInterval(timerId)
+      // }
+    }, [today])
+    console.log(useEffect)
+    
+    // let hour = today.getHours()
+    // let minute = today.getMinutes()
+    // let seconde = today.getSeconds()
+    
     return (
-        <div>{hour}:{minute}</div>
+        //<div>{hour}:{minute}:{seconde}</div>
+        <div>
+          <button onClick={()=> {setToday(1)}}>push</button>
+          <h1>{today}</h1>
+        </div>
+        
     )
     
 }
